b8d115b7cac4bf2fec0607757930529b2d4f0133
Jon Manning <jon@secretlab.com.au>
2018-01-07 15:41:31 +1100

    ch11 code

---
+++ 11-scene-kit/SceneKit/SceneKit/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  SceneKit
+//
+//  Created by Jon Manning on 26/08/2014.
+//  Copyright (c) 2014 Secret Lab. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+}
+
